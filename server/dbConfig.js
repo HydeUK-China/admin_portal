3aabd834a86ffeb905fb1167dfb670e74f4fe046
Jasmine Hong <hao.liu.uk@gmail.com>
2020-09-01 00:01:08 +0100

    add prod database param

--- server/dbConfig.js
+++ server/dbConfig.js
@@ -1,9 +1,9 @@
 const config = {
     prod: {
-        // host: process.env.RDS_HOSTNAME,
-        // user: process.env.RDS_USERNAME,
-        // password: process.env.RDS_PASSWORD,
-        // port: process.env.RDS_PORT
+        host: process.env.RDS_HOSTNAME,
+        user: process.env.RDS_USERNAME,
+        password: process.env.RDS_PASSWORD,
+        port: process.env.RDS_PORT
     },
     dev: {
         host: process.env.RDS_HOSTNAME,
