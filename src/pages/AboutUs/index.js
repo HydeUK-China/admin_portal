552cc4449d229dde60a2fb5ce390c375d263462e
YangHong92 <370303434@qq.com>
2020-10-09 11:53:18 +0100

    fix aboutus contactus css issue

--- src/pages/AboutUs/index.js
+++ src/pages/AboutUs/index.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import { getRole } from '../../utils/utils';
-
 import Footer from '../../components/Footer';
 
-import '../../styles/main.css';
+import '../../styles/aboutus.css';
 
 export default class AboutUs extends Component {
     constructor(props) {
@@ -19,20 +18,22 @@ export default class AboutUs extends Component {
     render() {
       const { role } = this.state;  
         return (
-    <div>
-    <header id="header">
+                  <div>
+                    <section id="top" className="about-header">
+                      <div className="side-image">
+                      </div>
+                      <header id="header">
                         <div className="brand-container">
-                            <a className="brand" href="/">
-                            Hyde International Talents
-                        
-                            </a>
+                            <Link className="brand" to="/home">
+                                Hyde International Talents       
+                            </Link>
                         </div>
 
                         <nav className="main-nav">
-                            <NavLink className="nav-item" to="/home">Home</NavLink>
-                            <NavLink className="nav-item" to="/jobs">Jobs</NavLink>
-                            <NavLink className="nav-item" to="/aboutus">About</NavLink>
-                            <NavLink className="nav-item" to="/contactus">Contact</NavLink>
+                            <NavLink className="nav-item" to="/home" style={{color: 'black'}}>Home</NavLink>
+                            <NavLink className="nav-item" to="/jobs" style={{color: 'black'}}>Jobs</NavLink>
+                            <NavLink className="nav-item" to="/aboutus" style={{color: 'black'}}>About</NavLink>
+                            <NavLink className="nav-item" to="/contactus" style={{color: 'black'}}>Contact</NavLink>
                             <div className="sign-in">
                             {role === '__admin__' ?
                                 <NavLink className="nav-item user" to="/mgt/admin_dashboard">
@@ -50,125 +51,126 @@ export default class AboutUs extends Component {
                             </div>
                         </nav>
         
-                     </header>
-    <section>  
-      <div class="container-fluid">
-      <div class="row">
-        <div class="col-sm-12 col-md-6">
-        </div>
-        <div class="col-sm-12 col-md-6 main-container">
-          <span class="about-title_shape">
-          </span>
-          <div class="about-information">
-            <h2>Hyde International Talents</h2>
-          </div>
-        </div>
-        <div class="story-about-us">
-          <div class="our-story-title">
-            Our Story
-          </div>
-          <p class="our-story-para">
-            With years of experience in technology-intensive fields, HIT understands the struggles that every researcher
-            and professional faces when looking for collaborative opportunities and research-related support for their
-            projects. HIT aims to unite highly talented researchers, investors and professionals in scientific and
-            technological fields with the best possible opportunities and solutions sourced from numerous
-            industry-leading organisations and research partners all around the world . HIT believes in the power of
-            connections and is dedicated to expanding and maintaining its network of members through providing them with
-            customised solutions. HIT strives to help facilitate its members’ goals and aspirations.</p>
-        </div>
-      </div>
-    </div>
-    <span class="contactAbout-shape"></span>
-  </section>
+                      </header>
 
-  <section id="faq" class="faq-section">
-    <div class="container-fluid-faq-content-box">
-      <div class="row">
-        <div class="col-6">
-          <h5 class="faq-sub-title">Still have questions?</h5>
-          <h1 class="faq-main-title">FREQUENTLY ASKED QUESTIONS.</h1>
-        </div>
-        <div class="accordian">
-          <div id="first-question" class="accordian-item">
-            <a class="accordian-link" href="#first-question">Do I need to be concerned with my Intellectual property
-              right and the confidentiality of the information regarding my research project?
-              <i class="add-circle-outline">
-                <ion-icon name="add-circle-outline"></ion-icon>
-              </i>
-              <i class="close-outline">
-                <ion-icon name="close-outline"></ion-icon>
-              </i>
-            </a>
-            <div class="answer">
-              <p>
-                The candidate will have the autonomy to determine how much information about his/her project/idea s/he
-                is willing to share in the whole process. Hyde will not ask for any sensitive information. More
-                importantly, both Hyde and the candidate's future partner/investor will guarantee the intellectual
-                property (IP) of the candidate through further legal documents.
-              </p>
-            </div>
-          </div>
+                      <div className="container-fluid">
+                        <div className="row">
+                          <div className="col-sm-12 col-md-6">
+                          </div>
+                          <div className="col-sm-12 col-md-6 main-container">
+                            <span className="about-title_shape">
+                            </span>
+                            <div className="about-information">
+                              <h2>Hyde International Talents</h2>
+                            </div>
+                          </div>
+                          <div className="story-about-us">
+                            <div className="our-story-title">
+                              Our Story
+                            </div>
+                            <p className="our-story-para">
+                              With years of experience in technology-intensive fields, HIT understands the struggles that every researcher
+                              and professional faces when looking for collaborative opportunities and research-related support for their
+                              projects. HIT aims to unite highly talented researchers, investors and professionals in scientific and
+                              technological fields with the best possible opportunities and solutions sourced from numerous
+                              industry-leading organisations and research partners all around the world . HIT believes in the power of
+                              connections and is dedicated to expanding and maintaining its network of members through providing them with
+                              customised solutions. HIT strives to help facilitate its members’ goals and aspirations.</p>
+                          </div>
+                        </div>
+                      </div>
+                      <span className="contactAbout-shape"></span>
+                    </section>
 
-          <div id="second-question" class="accordian-item">
-            <a class="accordian-link" href="#second-question">How much do I get to say in the selection of collaboration
-              opportunities?
-              <i class="add-circle-outline">
-                <ion-icon name="add-circle-outline"></ion-icon>
-              </i>
-              <i class="close-outline">
-                <ion-icon name="close-outline"></ion-icon>
-              </i>
-            </a>
-            <div class="answer">
-              <p>
-                The candidate will be able to directly participate in the entire negotiation process with the potential
-                partners/investors and decide which offer(s) to accept. The candidate will have the full power in
-                determining which of his/her project/idea will be cooperated with his/her future partners/investor and
-                how。
-              </p>
-            </div>
-          </div>
+                    <section id="faq" className="faq-section">
+                      <div className="container-fluid-faq-content-box">
+                        <div className="row">
+                          <div className="col-6">
+                            <h5 className="faq-sub-title">Still have questions?</h5>
+                            <h1 className="faq-main-title">FREQUENTLY ASKED QUESTIONS.</h1>
+                          </div>
+                          <div className="accordian">
+                            <div id="first-question" className="accordian-item">
+                              <a className="accordian-link" href="#first-question">Do I need to be concerned with my Intellectual property
+                                right and the confidentiality of the information regarding my research project?
+                                <i className="add-circle-outline">
+                                  <ion-icon name="add-circle-outline"></ion-icon>
+                                </i>
+                                <i className="close-outline">
+                                  <ion-icon name="close-outline"></ion-icon>
+                                </i>
+                              </a>
+                              <div className="answer">
+                                <p>
+                                  The candidate will have the autonomy to determine how much information about his/her project/idea s/he
+                                  is willing to share in the whole process. Hyde will not ask for any sensitive information. More
+                                  importantly, both Hyde and the candidate's future partner/investor will guarantee the intellectual
+                                  property (IP) of the candidate through further legal documents.
+                                </p>
+                              </div>
+                            </div>
 
-          <div id="third-question" class="accordian-item">
-            <a class="accordian-link" href="#third-question">Would get any help with the work visa application?
-              <i class="add-circle-outline">
-                <ion-icon name="add-circle-outline"></ion-icon>
-              </i>
-              <i class="close-outline">
-                <ion-icon name="close-outline"></ion-icon>
-              </i>
-            </a>
-            <div class="answer">
-              <p>
-                Visa sponsorship is guaranteed. Additionally, Hyde will offer relevant information and advice regarding
-                the visa application and will provide a certain level of support to the candidates.
-              </p>
-            </div>
-          </div>
+                            <div id="second-question" className="accordian-item">
+                              <a className="accordian-link" href="#second-question">How much do I get to say in the selection of collaboration
+                                opportunities?
+                                <i className="add-circle-outline">
+                                  <ion-icon name="add-circle-outline"></ion-icon>
+                                </i>
+                                <i className="close-outline">
+                                  <ion-icon name="close-outline"></ion-icon>
+                                </i>
+                              </a>
+                              <div className="answer">
+                                <p>
+                                  The candidate will be able to directly participate in the entire negotiation process with the potential
+                                  partners/investors and decide which offer(s) to accept. The candidate will have the full power in
+                                  determining which of his/her project/idea will be cooperated with his/her future partners/investor and
+                                  how。
+                                </p>
+                              </div>
+                            </div>
 
-          <div id="fourth-question" class="accordian-item">
-            <a class="accordian-link" href="#fourth-question"> Do I have to be physically working abroad?
-              <i class="add-circle-outline">
-                <ion-icon name="add-circle-outline"></ion-icon>
-              </i>
-              <i class="close-outline">
-                <ion-icon name="close-outline"></ion-icon>
-              </i>
-            </a>
-            <div class="answer">
-              <p>
-                The work location can be negotiated with future investors or partners. Not all of the research projects
-                and work opportunities require the researchers to physically be in China. Therefore, candidates have the
-                option to remain in their current positions and work remotely on a project/part-time basis.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-  <Footer/>
-  </div>
+                            <div id="third-question" className="accordian-item">
+                              <a className="accordian-link" href="#third-question">Would get any help with the work visa application?
+                                <i className="add-circle-outline">
+                                  <ion-icon name="add-circle-outline"></ion-icon>
+                                </i>
+                                <i className="close-outline">
+                                  <ion-icon name="close-outline"></ion-icon>
+                                </i>
+                              </a>
+                              <div className="answer">
+                                <p>
+                                  Visa sponsorship is guaranteed. Additionally, Hyde will offer relevant information and advice regarding
+                                  the visa application and will provide a certain level of support to the candidates.
+                                </p>
+                              </div>
+                            </div>
+
+                            <div id="fourth-question" className="accordian-item">
+                              <a className="accordian-link" href="#fourth-question"> Do I have to be physically working abroad?
+                                <i className="add-circle-outline">
+                                  <ion-icon name="add-circle-outline"></ion-icon>
+                                </i>
+                                <i className="close-outline">
+                                  <ion-icon name="close-outline"></ion-icon>
+                                </i>
+                              </a>
+                              <div className="answer">
+                                <p>
+                                  The work location can be negotiated with future investors or partners. Not all of the research projects
+                                  and work opportunities require the researchers to physically be in China. Therefore, candidates have the
+                                  option to remain in their current positions and work remotely on a project/part-time basis.
+                                </p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </section>
+                    
+                    <Footer/>
+                  </div>
         )
     }
 }
--- src/pages/ContactUs/index.js
+++ src/pages/ContactUs/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import { getRole } from '../../utils/utils';
-import '../../styles/main.css';
+import Footer from '../../components/Footer';
+
+import '../../styles/contactus.css';
 
 export default class ContactUs extends Component {
     constructor(props) {
@@ -16,20 +18,22 @@ export default class ContactUs extends Component {
     render() {
       const { role } = this.state;
         return (
-  <div>
-  <header id="header">
+                <div>
+                  <section id="top" className="contact-header">
+                      <div className="side-image">
+                      </div>
+                      <header id="header">
                         <div className="brand-container">
-                            <a className="brand" href="/">
-                            Hyde International Talents
-                        
-                            </a>
+                            <Link className="brand" to="/home">
+                                Hyde International Talents       
+                            </Link>
                         </div>
 
                         <nav className="main-nav">
-                            <NavLink className="nav-item" to="/home">Home</NavLink>
-                            <NavLink className="nav-item" to="/jobs">Jobs</NavLink>
-                            <NavLink className="nav-item" to="/aboutus">About</NavLink>
-                            <NavLink className="nav-item" to="/contactus">Contact</NavLink>
+                            <NavLink className="nav-item" to="/home" style={{color: 'black'}}>Home</NavLink>
+                            <NavLink className="nav-item" to="/jobs" style={{color: 'black'}}>Jobs</NavLink>
+                            <NavLink className="nav-item" to="/aboutus" style={{color: 'black'}}>About</NavLink>
+                            <NavLink className="nav-item" to="/contactus" style={{color: 'black'}}>Contact</NavLink>
                             <div className="sign-in">
                             {role === '__admin__' ?
                                 <NavLink className="nav-item user" to="/mgt/admin_dashboard">
@@ -46,45 +50,48 @@ export default class ContactUs extends Component {
                             }                          
                             </div>
                         </nav>
-        
                      </header>
-      <div class="container">
-      <div class="row">
-        <div class="col-sm-12 col-md-6">
-          <div class="page-title_content">
-            <h1>Contact <span style={{display:'block;'}}>us</span></h1>
-          </div>
-        </div>
-        <div class="col-sm-12 col-md-6 margin-top">
-          <h4 class="contact-form_header">Let us know we can help you.</h4>
-          <form>
-            <div class="form-group">
-              <label for="firstName">First Name</label>
-              <input type="text" class="form-control" id="exampleInputPassword1" placeholder="First name" />
-            </div>
-            <div class="form-group">
-              <label for="lastName">Last Name</label>
-              <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Last name" />
-            </div>
-            <div class="form-group">
-              <label for="Email">Email address</label>
-              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                placeholder="Enter email" />
-            </div>
-            <div class="form-group">
-              <label for="subject">Subject</label>
-              <input type="text" class="form-control" id="inputSubject" aria-describedby="emailHelp"
-                placeholder="Enter subject" />
-            </div>
-            <div class="form-group">
-              <textarea class="form-control" id="commentTextArea" rows="3" placeholder="Message"></textarea>
-            </div>
-            <button type="submit" class="btn btn-primary">Send</button>
-          </form>
-        </div>
-      </div>
-    </div>
-</div>
+
+                    <div className="container">
+                      <div className="row">
+                        <div className="col-sm-12 col-md-6">
+                          <div className="page-title_content">
+                            <h1>Contact <span style={{display:'block'}}>us</span></h1>
+                          </div>
+                        </div>
+                        <div className="col-sm-12 col-md-6 margin-top">
+                          <h4 className="contact-form_header">Let us know we can help you.</h4>
+                          <form>
+                            <div className="form-group">
+                              <label>First Name</label>
+                              <input type="text" className="form-control" id="exampleInputPassword1" placeholder="First name" />
+                            </div>
+                            <div className="form-group">
+                              <label>Last Name</label>
+                              <input type="text" className="form-control" id="exampleInputPassword1" placeholder="Last name" />
+                            </div>
+                            <div className="form-group">
+                              <label>Email address</label>
+                              <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                                placeholder="Enter email" />
+                            </div>
+                            <div className="form-group">
+                              <label>Subject</label>
+                              <input type="text" className="form-control" id="inputSubject" aria-describedby="emailHelp"
+                                placeholder="Enter subject" />
+                            </div>
+                            <div className="form-group">
+                              <textarea className="form-control" id="commentTextArea" rows="3" placeholder="Message"></textarea>
+                            </div>
+                            <button type="submit" className="btn btn-primary">Send</button>
+                          </form>
+                        </div>
+                      </div>
+                    </div>
+                  </section>
+
+                  <Footer />
+                </div>
         )
     }
 }
