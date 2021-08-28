7d0962c35f4edc1a38b8508aea42ab58a07b5bd5
mar1n <30738216+mar1n@users.noreply.github.com>
2021-08-28 21:28:26 +0100

    responsive chart

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,10 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { fetchReq } from "../../utils/utils";
 import "../../styles/dashboard.css";
+import _ from "lodash";
 import { Bar, Doughnut, Pie } from "react-chartjs-2";
 import ReactGA from "react-ga";
 
 const Dashboard = () => {
+  const barRef = useRef();
   const [totalApplicant, setTotalApplicant] = useState([]);
   const [gender, setGender] = useState([]);
   const [expertise, setExpertise] = useState([]);
@@ -16,7 +18,6 @@ const Dashboard = () => {
   const [expertise_val, setExpertise_val] = useState([]);
   const [nationality_val, setNationality_val] = useState([]);
   const [sourceref_val, setSourceref_val] = useState([]);
-
 
   useEffect(() => {
     ReactGA.pageview(window.location.pathname + window.location.search);
@@ -39,7 +40,7 @@ const Dashboard = () => {
         ] = data;
         setTotalApplicant(totalApplicant.map((ta) => ta.total_applicant));
         setGender(gender.map((g) => g.gender));
-        setGender_val(gender.map((g) => g.number_applicant))
+        setGender_val(gender.map((g) => g.number_applicant));
         setCategory(category.map((c) => c.category));
         setCategory_val(category.map((cv) => cv.number_applicant));
         setExpertise(expertise.map((e) => e.expertise));
@@ -48,32 +49,98 @@ const Dashboard = () => {
         setNationality_val(nationality.map((nv) => nv.number_applicant));
         setSourceref(sourceref.map((s) => s.source_references));
         setSourceref_val(sourceref.map((sv) => sv.number_references));
+
+        let nat = nationality.map((n) =>
+          n.nationality === "" || n.nationality === null
+            ? "France"
+            : n.nationality
+        );
+        let nat_val = nationality.map((nv) => nv.number_applicant);
+
+        var index = 11;
+        if (nat_val.length >= 10) {
+          var interval = setInterval(function () {
+            if (nat_val.length === index ) {
+              index = 0;
+            } else {
+              if(!barRef.current) {
+                clearInterval(interval);
+              } else {
+                const chart = barRef.current.chartInstance;
+                function addData(chart) {
+                  chart.data.datasets[0].data.shift();
+                  chart.data.labels.shift();
+                  chart.data.labels.push(nat[index]);
+                  chart.data.datasets[0].data.push(nat_val[index]);
+                  chart.update();
+                  index++;
+                }
+                addData(chart);
+              }
+            }
+          }, 2000);
+        }
       })
       .catch((err) => console.log(err));
   }, []);
 
+  const dataChart = (canvas) => {
+    const ctx = canvas.getContext("2d");
+    const gradient = ctx.createLinearGradient(0, 0, 0, 500);
+    gradient.addColorStop(0, "rgba(250,174,50,1)");
+    gradient.addColorStop(1, "rgba(250,174,50,0)");
+    // console.log('nationality',nationality);
+    // console.log('nationality_val', nationality_val);
+    let labels;
+    let data;
+
+    if (nationality.length > 10) {
+      labels = _.dropRight(nationality, nationality.length - 10);
+      data = _.dropRight(nationality_val, nationality_val.length - 10);
+    } else {
+      labels = nationality;
+      data = nationality_val;
+    }
+    return {
+      labels: labels,
+      datasets: [
+        {
+          backgroundColor: gradient, // Put the gradient here as a fill color
+          borderColor: "#ff6c23",
+          borderWidth: 2,
+          pointColor: "#fff",
+          pointStrokeColor: "#ff6c23",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "#ff6c23",
+          data: data,
+        },
+      ],
+    };
+  };
   return (
     <div className="dashboard">
       <section className="chart-one">
-        <div className='number_chart'>
+        <div className="number_chart">
           <h3>Total Applicant</h3>
           <hr />
-          <div className='no_font'>{totalApplicant}</div>
+          <div className="no_font">{totalApplicant}</div>
         </div>
 
-        <div className='gender_chart'>
+        <div className="gender_chart">
           <Pie
             data={{
               labels: gender,
-              datasets: [{
-                data: gender_val,
-                label: 'Gender',
-                backgroundColor: ['blue', 'red'],
-                borderWidth: 4,
-                borderColor: '#777',
-                hoverBorderWidth: 3,
-                hoverBorderColor: '#000'
-              }]
+              datasets: [
+                {
+                  data: gender_val,
+                  label: "Gender",
+                  backgroundColor: ["blue", "red"],
+                  borderWidth: 4,
+                  borderColor: "#777",
+                  hoverBorderWidth: 3,
+                  hoverBorderColor: "#000",
+                },
+              ],
             }}
             options={{
               // {
@@ -84,69 +151,72 @@ const Dashboard = () => {
               //       }
               //     }]
               //   },
-              responsive: false,
+              responsive: true,
               title: {
                 display: true,
-                text: 'Gender',
-                fontSize: 25
+                text: "Gender",
+                fontSize: 25,
               },
               legend: {
                 display: false,
               },
-              maintainAspectRatio:false
+              maintainAspectRatio: false,
             }}
-            height={300}
-            width={300}
           />
         </div>
-
-
-        <div className='nationality_chart'>
-
-          <Bar data={{
-            labels: nationality,
-            datasets: [{
-              data:nationality_val,
-              label: 'Nationality',
-              // backgroundColor: ['blue', 'red'],
-              borderWidth: 4,
-              borderColor: '#777',
-              hoverBorderWidth: 3,
-              hoverBorderColor: '#000'
-            }]
-          }}
-            options={
-              {
-                scales: {
-                  yAxes: [{
+        <div className="nationality_chart">
+          <Bar
+            ref={barRef}
+            id="bar-chart"
+            data={dataChart}
+            options={{
+              indexAxis: 'y',
+              scales: {
+                yAxes: [
+                  {
                     ticks: {
-                      min: 0
-                    }
-                  }]
-                },
-                responsive: true,
-                title: {
-                  display: true,
-                  text: 'Nationality',
-                  fontSize: 25
-                },
-                legend: {
-                  display: false,
-
-                },
-                maintainAspectRatio:false
-              }
-            }
-            height={400}
-            width={500}
+                      min: 0,
+                      max: 100,
+                    },
+                  },
+                ],
+                xAxes: [
+                  {
+                    ticks: {
+                      callback: function (value) {
+                        if (value && value.length > 4) {
+                          return value.substr(0, 2)  //truncate
+                        }  else {
+                          return value.substr(0, 2);
+                        }
+                      },
+                      fontSize: 10,
+                      padding: 5,
+                      autoSkip:true,
+                      maxRotation: 0,
+                      minRotation: 0,
+                      // maxTicksLimit: 2
+                    },
+                  },
+                ],
+              },
+              responsive: true,
+              title: {
+                display: true,
+                text: "Nationality",
+                fontSize: 25,
+              },
+              legend: {
+                display: false,
+              },
+              maintainAspectRatio: false,
+            }}
           />
         </div>
-
-
+        
       </section>
 
-
-      <section className="chart-two">
+      {/* <section className="chart-two">
         <div className="category_chart" >
           
           <Pie data={{
@@ -212,7 +282,7 @@ const Dashboard = () => {
                 //     }
                 //   }]
                 // },
-                responsive: false,
+                responsive: true,
                 title: {
                   display: true,
                   text: 'Expertise',
@@ -255,7 +325,7 @@ const Dashboard = () => {
                     }
                   }]
                 },
-                responsive: false,
+                responsive: true,
                 title: {
                   display: true,
                   text: 'Source Referrences',
@@ -272,11 +342,10 @@ const Dashboard = () => {
             width={600}
           />
         </div>
-      </section>
-
+      </section> */}
 
     </div>
-  )
+  );
 };
 
 export default Dashboard;
